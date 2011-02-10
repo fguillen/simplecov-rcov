3d463dde87b4dd7f1c4704248278fe5550c958d6
Wes Morgan <cap10morgan@gmail.com>
2011-02-10 15:34:58 -0500

    use the same coverage calculation as simplecov-html

--- lib/simplecov-rcov.rb
+++ lib/simplecov-rcov.rb
@@ -6,7 +6,7 @@ class SimpleCov::Formatter::RcovFormatter
 
     @total_lines =  result.files.map { |e| e.lines.count }.inject(:+)
     @total_lines_code =  result.files.map { |e| e.covered_lines.count + e.missed_lines.count }.inject(:+)
-    @total_coverage = result.covered_percent
+    @total_coverage = coverage(result.files)
 
     template = ERB.new( File.read( "#{File.dirname(__FILE__)}/../views/index.erb.html" ) )
     rcov_result = template.result( binding )
@@ -24,16 +24,18 @@ class SimpleCov::Formatter::RcovFormatter
 
   private
 
-    # Computes the coverage based upon lines covered and lines missed
-    def self.total_coverage(file_list)
-      return 100.0 if file_list.length == 0
-      porcents_sum = file_list.map(&:covered_percent).reduce(&:+)
-      result = porcents_sum / file_list.length
+  def lines_of_code(file_list)
+    lines_missed(file_list) + lines_covered(file_list)
+  end
 
-      return result
-    end
+  def coverage(file_list)
+    return 100.0 if file_list.length == 0 or lines_of_code(file_list) == 0
+    lines_missed = file_list.map {|f| f.missed_lines.count }.inject(&:+)
 
-    def self.path_result
-      File.join( SimpleCov.coverage_path, SimpleCov::Formatter::RcovFormatter::PATH_RESULT )
-    end
+    lines_covered(file_list) * 100 / lines_of_code(file_list).to_f
+  end
+
+  def self.path_result
+    File.join( SimpleCov.coverage_path, SimpleCov::Formatter::RcovFormatter::PATH_RESULT )
+  end
 end
